b8e169f448a2cd16f864be7a0bab784d216df66f
Daniel Interiano <dannyboyint@gmail.com>
2022-03-07 00:41:35 -0500

    Customized the manga cards, and updated the like button to show a user like it

--- src/components/Manga/Manga.js
+++ src/components/Manga/Manga.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import "./Manga.css"
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import "font-awesome/css/font-awesome.min.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "font-awesome/css/font-awesome.min.css"
 // import ReactStars from 'react-stars';
 import MangaForm from '../MangaForm/MangaForm';
 import { NavLink } from 'react-router-dom';
@@ -17,7 +17,6 @@ export default function Manga() {
     fetch("http://localhost:9292/mangas")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
         setData(data)})
         
       setLoading(false);
@@ -39,23 +38,27 @@ export default function Manga() {
     const updatedMangas = data.filter((manga) => manga.id !== idd);
     setData(updatedMangas);
   }
-  
+  function handleLikeClick(event){
+    console.log(event.target)
+    const liked = event.target
+    liked.innerText = "LOVED IT!"
+  }
   const ShowManga = () => {
     return (
       <div className='card-container'>
         {data.map((manga) => {
           return (
-            <div key={manga.id} className="manga-card">
-              <img src={manga.image} className="manga-card-img" alt={manga.title} />
+            <div key={manga.id} className="card">
+              <img src={manga.image} className="card-img" alt={manga.title} />
               <div className='card-body'>
-                <p className='card-title'>{manga.title}</p>
+                <h2 className='card-title'>{manga.title}</h2>
                 <p>by: {manga.author}</p>
                 <p>Publisher: {manga.publisher}</p>
-                <p>{manga.activity}</p>
+                <p>New Chapter: {manga.activity}</p>
                 <NavLink to={`/review`} className="rate-btn">Rate</NavLink>
                 {/* <ReactStars /> */}
                 <div>
-                  <button className='btn' id="like-btn">Like</button>
+                  <button className='btn' id="like-btn" onClick={handleLikeClick}>Like</button>
                   <button className='btn' id="delete-btn" name={manga.id} onClick={handleDeleteClick}>Delete</button>
                 </div>
               </div>
